22a3e7f5263922a6d0958b7c87f654784a8c1e08
CiprianoBryan <ciprianot.bryan@gmail.com>
2018-03-16 12:07:11 -0500

    add marker

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,7 +1,15 @@
 var map;
 function initMap() {
+    var coordinate = {
+        lat: -12.1220128,
+        lng: -77.0324022
+    }
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -12.1220128, lng: -77.0324022},
+        center: coordinate,
         zoom: 17
+    });
+    var marker = new google.maps.Marker({
+        position: coordinate,
+        map: map
     });
 }
